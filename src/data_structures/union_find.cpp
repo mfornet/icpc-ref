c600f2ea2f7c57a34f2bf801169fd071c2827f61
dgc9715 <dgc9715@gmail.com>
2020-02-28 03:44:12 -0500

    rectangle_union test + union_find doc

--- src/data_structures/union_find.cpp
+++ src/data_structures/union_find.cpp
@@ -1,3 +1,9 @@
+/*
+	Notes: Be careful with compression path when you can't use it (example: for undo)
+	
+	Complexity: O(a(n)) with compression path
+				O(log n) without compression path
+*/
 struct union_find
 {
 	vector<int> p;
@@ -15,7 +21,6 @@ struct union_find
 		return true;
 	}
 
-	// be careful with compression path when you can't use it (example: for undo)
 	int root(int u) { return p[u] < 0 ? u : p[u] = root(p[u]); }
 	int size(int u) { return -p[root(u)]; }
 };
---
+++ tests/geometry/rectangle_union.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DSL_4_A"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ll> point;
+#define F first
+#define S second
+
+#include "../../src/geometry/rectangle_union.cpp"
+
+int main()
+{
+	ios_base::sync_with_stdio(0), cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<rectangle> v(n);
+	for (auto &i : v)
+	{
+		cin >> i.xl >> i.yl >> i.xh >> i.yh;
+	}
+	cout << rectangle_area(v) << "\n";
+
+	return 0;
+}
